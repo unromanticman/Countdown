30b1a39856fb16624e1a228d4ff59a9385ef1203
yu hsiang <lin@test.com>
2021-02-27 22:33:54 +0800

    init

---
+++ index.js
@@ -0,0 +1,39 @@
+const moment = window.moment
+const { ipcRenderer } = require('electron')
+
+// gobal current second 
+let currentSecond = 50*60
+
+// helper function 
+function secondConvertHHMMSS(sec){
+  return moment("1900-01-01 00:00:00").add(sec, 'seconds').format("HH:mm:ss") 
+}
+
+function updateScreenTime(sec){
+  document.getElementById('time').innerHTML = secondConvertHHMMSS(sec)
+}
+
+function countdownTimer(){
+  let timer = setInterval(() => {
+    // remove one second
+    currentSecond = currentSecond - 1
+    // refresh screen
+    updateScreenTime(currentSecond)
+
+    // when time reaching 0
+    if (currentSecond <= 0) {
+      clearInterval(timer)
+    }
+    ipcRenderer.send('resize-me-please')
+
+  }, 1000)
+}
+
+// main
+updateScreenTime(currentSecond)
+document.getElementById('startBtn').addEventListener("click",function(){
+  countdownTimer()  
+  document.getElementById('startBtn').style.display = "none"
+  document.getElementById('countdownText').style.display = "block"
+})
+
